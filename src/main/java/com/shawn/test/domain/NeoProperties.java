d1e03332f166ff0663ea94e6c43943e9b2e33fdc
shawn <821519730@qq.com>
2017-10-30 15:24:41 +0800

    seconde commit

--- src/main/java/com/shawn/test/domain/NeoProperties.java
+++ src/main/java/com/shawn/test/domain/NeoProperties.java
@@ -3,6 +3,8 @@ package com.shawn.test.domain;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+
+
 @Component
 public class NeoProperties {
 	@Value("${com.neo.title}")
